2de64a369d278945f82b91047e72a1ee8cfb85d8
SHerdenZ <zuhio76@gmail.com>
2021-01-22 13:25:58 +0400

    fear: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable (number = 0) {
+    let units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let dec = ['twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+    let hun = 'hundred';
+    let txtHun = '';
+    let txtDec = '';
+    let txtUn = '';
   
+    let numHun = Math.floor((number / 100));
+    let numDec = Math.floor((number % 100) / 10);
+    let numUn = number % 10;
+  
+    if (number == 0) return 'zero';
+
+    if (numHun < 1) {
+      txtHun = '';
+    } else {
+      txtHun = units[numHun] + ' hundred';
+    };
+  
+    if (numDec< 1) {
+      txtDec = '';
+    } else if (numDec > 1) {
+      txtDec = dec[numDec-2];
+    } else {
+      txtDec = units[10+numUn];
+      txtUn = '';
+    }
+    
+    if (numDec != 1) {
+        if (numUn != 0) {
+            txtUn = units[numUn];
+        }
+    }
+  
+    let result = txtHun + ' '+ txtDec + txtUn;
+    return result.trim()
 }
